1842ea45deac90e9ea718762cb3cf8aac024459d
shin <nextstagexx@gmail.com>
2020-09-15 00:58:38 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,32 @@
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-Rails.application.routes.draw do
-  root 'application#hgit ome'
-  
-  get 'static_pages/home'
+# Rails.application.routes.draw do
+  # get 'users/new'
 
-  get 'static_pages/help'
+#   # root 'application#hello'
+#   root 'static_pages#home'
+
+#   get 'static_pages/home'
+
+#   get 'static_pages/help'
   
-  get 'static_pages/about'
+#   get 'static_pages/about'
   
+#   get 'static_pages/contact'
+
+# end
+
+Rails.application.routes.draw do
+  # get 'users/new'
+
+  # root 'application#hello'
+  root 'static_pages#home'
+
+
+  get '/help', to: 'static_pages#help'
+  
+  get '/about', to: 'static_pages#about'
+  
+  get '/contact', to: 'static_pages#contact'
+  
+  get '/signup', to: 'users#new'
+
 end
